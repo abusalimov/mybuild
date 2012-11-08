f2a83ab643b70fb8c6aee5440430dd67687b11b7
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-11-08 10:20:19 +0000

    Add a simple test case for Constraints.

--- mybuild/test/test_core.py
+++ mybuild/test/test_core.py
@@ -2,6 +2,7 @@
 Tests for Mybuild core.
 """
 
+import unittest
 from unittest import TestCase
 # from mock import Mock, patch
 
@@ -48,8 +49,29 @@ class ModuleTestCase(TestCase):
 class ConstraintsTestCase(TestCase):
     """Tests for Constraints and friends."""
 
-    def test_xxx(self):
-        pass
+    def test_no_forks(self):
+        @module
+        def m(self, foo, bar):
+            pass
+
+        c = Constraints()
+
+        self.assertIsNone(c.check(m))
+        self.assertIsNone(c.check(m, 'foo', 42))
+        self.assertIsNone(c.check(m, 'foo', 17))
+
+        c.constrain(m, 'foo', 17, negated=True)
+
+        self.assertIsNone(c.check(m))
+        self.assertIsNone(c.check(m, 'foo', 42))
+        self.assertFalse(c.check(m, 'foo', 17))
+
+        c.constrain(m, 'foo', 42)
+
+        self.assertTrue(c.check(m))
+        self.assertTrue(c.check(m, 'foo', 42))
+        self.assertFalse(c.check(m, 'foo', 17))
+
 
 if __name__ == '__main__':
     unittest.main()
