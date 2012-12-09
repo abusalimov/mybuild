0425cdf19cb6ca709fe50e1419a5809a22c08e4b
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-12-09 23:55:52 +0000

    Move auxillary classes from mybuild.core into util package

--- mybuild/util.py
+++ mybuild/util.py
@@ -18,3 +18,74 @@ def unique(iterable, key=id):
             seen_add(k)
             yield element
 
+
+class InstanceBoundTypeMixin(object):
+    """
+    Base class for per-instance types, that is types defined for each instance
+    of the target type.
+
+    Do not use without mixing in a instance-private type.
+    """
+    __slots__ = ()
+
+    # These may be too strict checks, but it is OK,
+    # since types are mapped to the corresponding instances one-to-one.
+    _type_eq   = classmethod(lambda cls, other: cls is type(other))
+    _type_hash = classmethod(id)
+
+    def __eq__(self, other):
+        return self._type_eq(type(other))
+    def __hash__(self):
+        return self._type_hash()
+
+
+class DynamicAttrsMixin(object):
+    """
+    Provides a facility for other types to register themselves to be notified
+    each time when a new instance of this type gets created.
+    """
+
+    class __metaclass__(type):
+        # TODO mixing in a metaclass looks like a not goog idea... -- Eldar
+
+        def __init__(cls, *args, **kwargs):
+            type.__init__(cls, *args, **kwargs)
+            cls._registered_attrs = {} # attr-to-method
+
+        def register_attr(cls, attr, factory_method='_new_type'):
+            """Deco maker for per-module classes."""
+
+            if not isinstance(attr, basestring):
+                raise TypeError("Attribure name must be a string, "
+                                "got %s object instead: %r" %
+                                (type(attr), attr))
+
+            if not attr.startswith('_'):
+                raise ValueError("Attribute name must start with underscore")
+
+            def deco(target):
+                if not isinstance(target, type):
+                    raise TypeError("'@%s.register_attr(...)' must be applied "
+                                    "to a class, got %s object instead: %r" %
+                                    (cls.__name__, type(target), target))
+
+                if not hasattr(target, factory_method):
+                    raise TypeError("'@%s.register_attr(...)'-decorated class "
+                                    "must define a '%s' classmethod" %
+                                    (cls.__name__, factory_method))
+
+                if hasattr(cls, attr) or attr in cls._registered_attrs:
+                    raise ValueError("%s class already has attribure '%s'" %
+                                     (cls.__name__, attr))
+
+                cls._registered_attrs[attr] = getattr(target, factory_method)
+
+                return target
+
+            return deco
+
+    def _init_attrs(self, *args, **kwargs):
+        for cls in type(self).__mro__:
+            if isinstance(cls, self.__metaclass__):
+                for attr, factory in cls._registered_attrs.iteritems():
+                    setattr(self, attr, factory(*args, **kwargs))
