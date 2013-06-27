772c3a0d869da3189fc0f00d5eaac2fd9582804c
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-06-27 12:22:17 +0000

    some changes in mywaf.load_myfiles

--- mybuild/mywaf.py
+++ mybuild/mywaf.py
@@ -3,6 +3,7 @@ Mybuild tool for Waf.
 """
 
 from waflib import Context as wafcontext
+from waflib import Utils   as wafutils
 
 def options(ctx):
 	print('mywaf: %r' % ctx)
@@ -10,7 +11,11 @@ def options(ctx):
 def configure(ctx):
 	print('mywaf: %r' % ctx)
 
-def load_myfiles(ctx, files, package_root):
-	pass
+def load_myfiles(ctx, myfile_names, root_node=None):
+	if root_node is None:
+		root_node = ctx.path
+	myfiles_glob = ['**/' + f for f in wafutils.to_list(myfile_names)]
+	files = root_node.ant_glob(myfiles_glob)
+	print files
 
 wafcontext.Context.load_myfiles = load_myfiles
