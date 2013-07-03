4e50ce659ba9f622f0ee30e14ee0f3053d240aca
Anton Bondarev <anton.bondarev2310@gmail.com>
2013-07-03 14:05:09 +0000

    generate 'waf' task in each module directory

--- mywaf/model.py
+++ mywaf/model.py
@@ -1,3 +1,5 @@
+import os
+
 from mybuild import loader
 from mybuild.context import Context, InstanceAtom
 from mybuild.solver import solve
@@ -10,22 +12,31 @@ from waflib.TaskGen import feature, extension, after_method
 from waflib.Tools import ccroot
 
 
+def add_mytask(bld, pnode):
+    mod = pnode.instance.module
+    fullfile = mod._file
+    fnode = bld.root.find_node(fullfile)
+    
+    bld.pre_recurse(fnode)
+    folder = fnode.parent
+    src = getattr(pnode.instance, 'sources', [])
+    bld(features='mylink', source=src, target='test')
+    print('+++++++++ add task ++++')
+    print('module = ' + str(mod))
+    print('folder = ' + folder.abspath())
+    print('sources = ' + str(src))
+    bld.post_recurse(fnode)
+    print('+++++++++++++++++++++++')
+    
+
 def build_true_graph(bld, solution):
     ret = dict()
     for pnode, value in iteritems(solution):
         if isinstance(pnode, InstanceAtom):
             if str(pnode) != 'prj.conf.PYBUILD.conf()':
                 if value == True:
-                    print '+++', pnode
-
-                    src = getattr(pnode.instance, 'sources', [])
-                    fullsrc = 'src/hello/' + str(src)
-                    bld(features='mylink', source=fullsrc, target='test')
-
-                    print(src)
-            else:
-                print '---', pnode
-
+                    add_mytask(bld, pnode)
+    
     return ret
 
 
@@ -37,7 +48,7 @@ def create_model(bld):
     prj = bld.my_load('prj', ['src', bld.env.TEMPLATE], get_defaults(), ['Pybuild'])
 
     ################################
-    conf = prj.conf.conf
+    conf = prj.conf.PYBUILD.conf
 
     context = Context()
     context.consider(conf)
@@ -47,16 +58,16 @@ def create_model(bld):
     solution = solve(g, {g.atom_for(conf):True})
 
     true_g = build_true_graph(bld, solution)
-
+    
 
 
 @after_method('process_source')
 @feature('mylink')
 def call_apply_link(self):
-        print(self)
-
+    print('link')
+    print(self)
+        
 @extension('.c')
 def process_ext(self, node):
-        #self.create_compiled_task('ext2o', node)
-        print(node)
-
+    #self.create_compiled_task('ext2o', node)
+    print(node)
