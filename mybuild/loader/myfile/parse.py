3ae5f2679df570655630352a60b98ee6bc3ba31d
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-07-07 22:43:36 +0000

    clean up myfiles parser a bit, add docs

--- mybuild/loader/myfile/parse.py
+++ mybuild/loader/myfile/parse.py
@@ -1,47 +1,13 @@
-from collections import namedtuple
-from operator import itemgetter
+"""
+PLY-based parser for My-files grammar. Also provides some scoping features.
+"""
+
 import ply.yacc
 
 from . import lex
 
-from ...util.compat import *
 from ...util.collections import OrderedDict
-
-
-class Object(object):
-    """docstring for Object"""
-
-    def __init__(self, parent=None):
-        super(Object, self).__init__()
-        self.parent = parent
-
-        self.init_header(None, {})
-        self.init_body({}, None)
-
-    def init_header(self, type_name, type_args, name=None):
-        self.type_name = type_name
-        self.type_args = type_args
-        self.__name__ = self.__qualname__ = name
-        if name and self.parent and self.parent.__qualname__:
-            self.__qualname__ = self.parent.__qualname__ + '.' + name
-
-    def init_body(self, attrs, docstring):
-        self.attrs = attrs
-        self.__doc__ = docstring
-
-    def __repr__(self):
-        return '{type} {name}({type_args}){attrs}'.format(
-                    type=self.type_name or '',
-                    name=self.__qualname__ or '',
-                    type_args=dict(self.type_args) or '',
-                    attrs=dict(self.attrs) or '')
-
-
-def to_rlist(reversed_list):
-    return reversed_list[::-1]
-
-def to_rdict(reversed_pairs):
-    return OrderedDict(reversed(reversed_pairs))
+from ...util.compat import *
 
 
 tokens = lex.tokens
@@ -126,24 +92,11 @@ def p_parameter(p):
 def p_object_body(p):
     """object_body : LBRACE enter_scope docstring object_members RBRACE"""
     this_object(p).init_body(attrs=to_rdict(p[4]), docstring=p[3])
-    p.parser.nesting_depth -= 1
+    leave_scope(p)
 
 def p_enter_scope(p):
     """enter_scope :"""
-    p.parser.nesting_depth += 1
-
-def scope_object(p):
-    return p.parser.object_stack[p.parser.nesting_depth]
-
-def this_object(p):
-    return p.parser.object_stack[-1]
-
-def push_object(p, o):
-    p.parser.object_stack.append(o)
-    return o
-
-def pop_object(p):
-    return p.parser.object_stack.pop()
+    enter_scope(p)
 
 
 def p_docstring_0(p):
@@ -191,7 +144,77 @@ def p_error(t):
 parser = ply.yacc.yacc(method='LALR', write_tables=False, debug=0)
 
 
+# Here go scoping-related stuff + some utils.
+
+class Object(object):
+    """docstring for Object"""
+
+    def __init__(self, parent=None):
+        super(Object, self).__init__()
+        self.parent = parent
+
+        self.init_header(None, {})
+        self.init_body({}, None)
+
+    def init_header(self, type_name, type_args, name=None):
+        self.type_name = type_name
+        self.type_args = type_args
+        self.__name__ = self.__qualname__ = name
+        if name and self.parent and self.parent.__qualname__:
+            self.__qualname__ = self.parent.__qualname__ + '.' + name
+
+    def init_body(self, attrs, docstring):
+        self.attrs = attrs
+        self.__doc__ = docstring
+
+    def __repr__(self):
+        return '{type} {name}({type_args}){attrs}'.format(
+                    type=self.type_name or '',
+                    name=self.__qualname__ or '',
+                    type_args=dict(self.type_args) or '',
+                    attrs=dict(self.attrs) or '')
+
+
+def to_rlist(reversed_list):
+    return reversed_list[::-1]
+
+def to_rdict(reversed_pairs):
+    return OrderedDict(reversed(reversed_pairs))
+
+
+def this_object(p):
+    return p.parser.object_stack[-1]
+def scope_object(p):
+    return p.parser.object_stack[p.parser.nesting_depth]
+
+def push_object(p, o):
+    p.parser.object_stack.append(o)
+    return o
+def pop_object(p):
+    return p.parser.object_stack.pop()
+
+def enter_scope(p):
+    p.parser.nesting_depth += 1
+def leave_scope(p):
+    p.parser.nesting_depth -= 1
+
+
+# The main entry point.
+
 def parse(text, **kwargs):
+    """
+    Parses the given text and returns the result.
+
+    Args:
+        text (str) - data to parse
+        **kwargs are passed directly to the underlying PLY parser
+
+    Returns a tuple (AST root, exports, references):
+      - AST root is always a list of values
+      - exports is a dict mapping qualified names to corresponding objects
+      - references is a list of (name, scope object) tuples
+    """
+
     parser.exports    = {}  # {qualname: object}
     parser.references = []  # (name, scope)
 
