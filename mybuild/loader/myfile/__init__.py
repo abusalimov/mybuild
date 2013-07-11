bdc1d2d756757a98ea9b3a7a0ee82d95ad4dde20
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-07-11 16:22:16 +0000

    some incomplete linkage-related drafts in myfile loader

--- mybuild/loader/myfile/__init__.py
+++ mybuild/loader/myfile/__init__.py
@@ -21,9 +21,42 @@ FILENAME = 'Mybuild'
 class MybuildFileLoader(SourceFileLoader):
     """Loads Mybuild files."""
 
-    def __init__(self, ctx, fullname, path):
+    @classmethod
+    def init_ctx(cls, ctx, initials):
+        return None  # FIXME linker object
+
+    @classmethod
+    def exit_ctx(cls, linker):
+        pass  # invoke global linkage here
+
+    def __init__(self, linker, fullname, path):
         super(MybuildFileLoader, self).__init__(fullname, path)
+        self.linker = linker
 
     def is_package(self, fullname):
         return False
+
+    def get_code(self, fullname):
+        return None
+
+    def _exec_module(self, module):
+        if ply is None:
+            raise ImportError('PLY is not installed')
+
+        try:
+            result = parse(self.get_source(module.__name__))
+
+        except IOError:
+            raise ImportError("IO error while reading a stream")
+        except SyntaxError:
+            raise
+
+        else:
+            print result
+            self._link_module(module, result)
+
+    def _link_module(self, module, result):
+        self.linker  # TODO tell linker about a new module
+
+
 
