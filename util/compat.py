e5bf46624b0d33f55327b0250c73cbbe023f0672
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-01 16:23:00 +0000

    replace compat.with_metaclass with an improved version derived from Jinja2

--- util/compat.py
+++ util/compat.py
@@ -22,6 +22,8 @@ else:
     from itertools import izip    as zip
     next = _operator.methodcaller('next')
 
+from itertools import ifilterfalse as filternot
+
 
 if _py3k:
     itervalues = lambda d: iter(d.values())
@@ -34,16 +36,52 @@ else:
 iterkeys = iter
 
 
-if _py3k:
-    def with_metaclass(meta):
-        return meta('MetaBase', (), dict(__slots__=()))
+def with_metaclass(*meta_arg, **kwargs):
+    if meta_arg:
+        meta, = meta_arg
+    else:
+        meta = None
+    # Derived from Jinja2.
+    #
+    # This requires a bit of explanation: the basic idea is to make a
+    # dummy metaclass for one level of class instantiation that replaces
+    # itself with the actual metaclass.  Because of internal type checks
+    # we also need to make sure that we downgrade the custom metaclass
+    # for one level to something closer to type (that's  __init__ comes
+    # back from type etc.).
+    #
+    # This has the advantage over six.with_metaclass in that it does not
+    # introduce dummy classes into the final MRO.
+    class metaclass(meta or type):
+        __init__ = type.__init__
+        def __new__(mcls, name, this_bases, attrs):
+            if this_bases is None:
+                return type.__new__(mcls, name, (), attrs)
+            bases = tuple(base for base in this_bases
+                          if base is not temp_class)
+            mcls = meta or compute_default_metaclass(bases)
+            return mcls(name, bases, attrs, **kwargs)
+    temp_class = metaclass('temp_class', None, {})
 
-else:
-    def with_metaclass(meta):
-        class MetaBase(object):
-            __metaclass__ = meta
-            __slots__ = ()
-        return MetaBase
+    return temp_class
+
+# derived from Py3k Lib/types.py
+def compute_default_metaclass(bases):
+    """Calculate the most derived metaclass."""
+    winner = type
+    for base in bases:
+        base_meta = type(base)
+        if issubclass(winner, base_meta):
+            continue
+        if issubclass(base_meta, winner):
+            winner = base_meta
+            continue
+        # else:
+        raise TypeError("metaclass conflict: "
+                        "the metaclass of a derived class "
+                        "must be a (non-strict) subclass "
+                        "of the metaclasses of all its bases")
+    return winner
 
 ABCBase = with_metaclass(_abc.ABCMeta)
 
