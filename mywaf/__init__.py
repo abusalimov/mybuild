c12b5f77d38569b0e2320ac29c69f104cc715c5f
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-07 16:28:09 +0000

    add waflib.Context.mybuild facade method which loads all DSL files, resolves a configuration and recurses into each instance being built

--- mywaf/__init__.py
+++ mywaf/__init__.py
@@ -4,38 +4,50 @@ Mybuild tool for Waf.
 
 import os.path
 
+from operator import attrgetter
+
 from waflib import Context as wafcontext
 from waflib import Utils   as wafutils
 from waflib import Errors  as waferrors
 
+from mybuild.context import resolve
+
+from glue import PyDslLoader
+from glue import MyDslLoader
+
 from nsimporter import import_all
 from nsimporter import loader_filename
 
+from util import is_mapping
+from util import instanceof
 from util.compat import *
 
 
+def init(ctx):
+    print('mywaf: init %r' % ctx)
+
 def options(ctx):
-    print('mywaf: %r' % ctx)
+    print('mywaf: options %r' % ctx)
 
 def configure(ctx):
-    print('mywaf: %r' % ctx)
+    print('mywaf: configure %r' % ctx)
 
 
-def my_load(ctx, namespace, path=None, loaders_init=None):
-    loaders_init = dict(loaders_init)
+DEFAULT_LOADERS = [MyDslLoader, PyDslLoader]
 
-    myfiles_glob = ['**/' + loader_filename(l) for l in loaders_init]
+def my_load(ctx, namespace, path=None, loaders=DEFAULT_LOADERS):
+    loaders_init = (dict if is_mapping(loaders) else dict.fromkeys)(loaders)
 
     if path is not None:
         path_nodes = [ctx.path.find_node(entry) for entry in path]
     else:
         path_nodes = [ctx.path]
-
     path = [node.abspath() for node in path_nodes]
 
-    found_names = sorted(set(found.parent.path_from(node)
+    loader_files_glob = ['**/' + loader_filename(l) for l in loaders_init]
+    found_rel_dirs = sorted(set(found.parent.path_from(node)
                              for node in path_nodes
-                             for found in node.ant_glob(myfiles_glob)))
+                             for found in node.ant_glob(loader_files_glob)))
 
     def no_dots(dirname):
         has_dot = ('.' in dirname)
@@ -45,7 +57,7 @@ def my_load(ctx, namespace, path=None, loaders_init=None):
                           .format(**locals()))
         return not has_dot
 
-    return import_all(filter(no_dots, found_names), namespace, path,
+    return import_all(filter(no_dots, found_rel_dirs), namespace, path,
                       loaders_init)
 
 wafcontext.Context.my_load = my_load
@@ -56,7 +68,7 @@ def my_recurse(ctx, instances, name=None, mandatory=True, once=True):
         name = ctx.fun
 
     for instance in instances:
-        node = ctx.root.find_node(instance.module._file)
+        node = ctx.root.find_node(instance._file)
 
         ctx.pre_recurse(node)
         try:
@@ -72,3 +84,63 @@ def my_recurse(ctx, instances, name=None, mandatory=True, once=True):
             ctx.post_recurse(node)
 
 wafcontext.Context.my_recurse = my_recurse
+
+
+def mybuild(ctx, conf_name, path=None, loaders=DEFAULT_LOADERS):
+    namespace, _, conf_relname = conf_name.partition('.')
+    if not conf_relname:
+        raise ValueError("conf_name must be a qualified name "
+                         "(including a namespace): '%s'" % conf_name)
+    else:
+        conf_getter = attrgetter(conf_relname)
+
+    ns_pymodule = ctx.my_load(namespace, path, loaders)
+    conf_module = conf_getter(ns_pymodule)
+
+    instances = resolve(conf_module, module_mixin=MyWafModuleMixin)
+    ctx.my_recurse(instances)
+
+    raise NotImplementedError
+
+    return next(filter(instanceof(conf_module), instances))
+
+wafcontext.Context.mybuild = mybuild
+
+
+class MyWafModuleMixin(object):
+    """docstring for MyWafModuleMixin"""
+
+    def __init__(self, *args, **kwargs):
+        print self, args, kwargs
+        self.sources = []
+        super(MyWafModuleMixin, self).__init__(*args, **kwargs)
+
+    def build(self, bld):
+        print('mywaf build: %r' % self)
+        print('\tsources: %r' % self.sources)
+        # bld(features='mylink', source=src, target='test')
+
+# try:
+#     from waflib.Task import Task
+#     from waflib.TaskGen import feature, extension, after_method
+#     from waflib.Tools import ccroot
+
+#     @after_method('process_source')
+#     @feature('mylink')
+#     def call_apply_link(self):
+#         print('linking' + str(self))
+
+#     class mylink(ccroot.link_task):
+#         run_str = 'cat ${SRC} > ${TGT}'
+
+#     class ext2o(Task):
+#         run_str = 'cp ${SRC} ${TGT}'
+
+#     @extension('.c')
+#     def process_ext(self, node):
+#         self.create_compiled_task('ext2o', node)
+
+# except ImportError:
+#     pass  # XXX move Waf-related stuff from here
+
+
