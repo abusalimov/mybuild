0fa530760d953d0b452e4f87005d0dc3a4abf84e
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-14 18:38:47 +0000

    solver: throw out ignore_errors logic, no need in sync_with_trunk anymore, Branch.flatten now takes into an account the revision of the trunk when the branch was updated last also expand_branch and resolve_branches functions are now much more clear and clean

--- mybuild/solver.py
+++ mybuild/solver.py
@@ -22,6 +22,7 @@ from mybuild.rgraph import *
 from util.itertools import pop_iter
 from util.operator import getter
 from util.operator import invoker
+from util.prop import cached_property
 
 import util, logging
 logger = util.get_extended_logger(__name__)
@@ -54,23 +55,22 @@ class Solution(object):
         del self.literals
         del self.reasons
 
-    def update(self, other, ignore_errors=False):
+    def __ior__(self, other):
         self.nodes    |= other.nodes
         self.literals |= other.literals
         self.reasons  |= other.reasons
+        return self
 
-        self._error_check(ignore_errors)
-
-    def difference_update(self, other, ignore_errors=False):
+    def __isub__(self, other):
         self.nodes    -= other.nodes
         self.literals -= other.literals
         self.reasons  -= other.reasons
+        return self
 
-        self._error_check(ignore_errors)
-
-    def _error_check(self, ignore_errors=False):
-        if not ignore_errors and not self.valid:
-            raise SolutionError(self)
+    def isdisjoint(self, other):
+        # note that reasons are not inspected
+        return (self.nodes    .isdisjoint(other.nodes) and
+                self.literals .isdisjoint(other.literals))
 
 
 class Trunk(Solution):
@@ -79,28 +79,56 @@ class Trunk(Solution):
     _dump_attrs = (Solution._dump_attrs +
                    'branchmap dead_branches neglefts'.split())
 
+    @cached_property
+    def base(self):
+        ret = Solution()
+
+        ret |= self
+        for diff in reversed(self.commits):
+            ret -= diff
+
+        return ret
+
+    @property
+    def rev(self):
+        return len(self.commits)
+
     def __init__(self):
         super(Trunk, self).__init__()
+
+        self.neglefts = dict()   # neglasts to sets of left literals
 
         self.branchmap     = dict()  # maps gen literals to branches
         self.dead_branches = dict()  # gen literals to dead branches
 
-        self.neglefts = dict()   # neglasts to sets of left literals
+        self.commits = list()  # incremental diffs applied to the trunk
 
-    def update(self, diff, ignore_errors=False):
+    def commit(self, diff):
         if self is not diff.trunk:
             raise ValueError('Diff must be created from this trunk')
+        if not (self.valid and diff.valid):
+            raise ValueError('Can only apply a valid diff')
 
-        assert self.literals.isdisjoint(diff.literals), \
-                "diff must not intersect the trunk (must be a strict diff)"
+        assert self.isdisjoint(diff), ("diff must not intersect the trunk "
+                                       "(must be a strict diff)")
+
+        for literal in diff.literals:
+            del self.branchmap[literal]  # just remove
+
+            refused_literal = ~literal
+            refused_branch = self.branchmap.pop(refused_literal)
+            self.dead_branches[refused_literal] = refused_branch
+
+            # Remember the current revision to be able to reproduce the branch
+            # later (for error reporting, e.g.).
+            refused_branch.baserev = self.rev
 
         for neglast, negexcl in iteritems(diff.negexcls):
             self.neglefts[neglast] -= negexcl
 
-        super(Trunk, self).update(diff, ignore_errors)
+        self |= diff
 
-    def difference_update(self, other, ignore_errors=False):
-        raise NotImplementedError('Unsupported operation')
+        self.commits.append(diff)  # self.rev gets incremented
 
     def substitute_branch(self, branch, other):
         """
@@ -114,18 +142,21 @@ class Trunk(Solution):
 
         other.gen_literals |= branch.gen_literals
 
-        assert branch.valid or not other.valid
-
-        # Even if other is not valid, use a branchmap for branch.
-        branchmap = (self.branchmap if branch.valid else
-                     self.dead_branches)
-
         # Fixup any references to this one.
         for gen_literal in branch.gen_literals:
-            assert branchmap[gen_literal] is branch
-            branchmap[gen_literal] = other
+            assert self.branchmap[gen_literal] is branch
+            self.branchmap[gen_literal] = other
 
-    def iter_branch_todo_away(self, branch, ignore_errors=False):
+    def iter_branch_todo_away(self, branch):
+        """
+        Exhausts a todo set of literals of the given branch, yielding them
+        among with corresponding branches.
+
+        Yields: (literal, branch) tuples. Neither literals included in the
+        branch nor in self are yielded (they are simply discarded from the
+        todo set).
+        Note: The iterator is modification-safe.
+        """
         if self is not branch.trunk:
             raise ValueError("Branch must be created from this trunk")
 
@@ -134,16 +165,9 @@ class Trunk(Solution):
                 implied = self.branchmap[literal]
 
             except KeyError:
-                if literal in self.literals:
-                    continue  # included in the trunk, i.e. unconditionally
                 assert ~literal in self.literals
-
-                if not ignore_errors:
-                    raise SolutionError(branch)
-
-                # If ~literal was added into trunk by create_trunk, then
-                # there is no branch for literal, even dead.
-                # Give up in this case and yield None.
+                # If ~literal was added by create_trunk, then there is
+                # no branch for literal, even dead. In this case yield None.
                 implied = self.dead_branches.get(literal)
 
             yield literal, implied
@@ -168,7 +192,9 @@ class Diff(Solution):
     def __init__(self, trunk):
         super(Diff, self).__init__()
 
-        self.trunk = trunk
+        self.trunk   = trunk
+        self.baserev = trunk.rev  # always 0 as long as all branches are
+                                  # created before any commit to trunk
 
         self.todo = set()  # literals
         self.negexcls = defaultdict(set)  # {neglast: literals...}
@@ -181,12 +207,19 @@ class Diff(Solution):
 
     def flatten(self):
         if not self.ready:
-            raise NotImplementedError('not ready: {0}: {0.todo}'.format(self))
+            raise ValueError('not ready: {0}: {0.todo}'.format(self))
+
+        trunk = self.trunk
+        if self.baserev == trunk.rev:
+            changesets = [trunk]
+        else:
+            changesets = [trunk.base] + trunk.commits[:self.baserev]
+        changesets.append(self)
 
         ret = Solution()
-        ret.update(self.trunk)
-        ret.update(self)
-        assert ret.valid == self.valid
+        for diff in changesets:
+            assert ret.isdisjoint(diff)
+            ret |= diff
 
         return ret
 
@@ -194,45 +227,33 @@ class Diff(Solution):
         if self.trunk is not other.trunk:
             raise ValueError('Both diffs must belong to the same trunk')
         if not other.ready:
-            raise NotImplementedError('not ready: {0}: {0.todo}'.format(other))
+            raise ValueError('not ready: {0}: {0.todo}'.format(other))
 
-    def update(self, other, ignore_errors=False):
+    def merge(self, other):
         self._check_capable(other)
 
         for neglast, negexcl in iteritems(other.negexcls):
             assert self.trunk.neglefts[neglast] >= negexcl
             self.__do_neglast(neglast, operator.__ior__, negexcl)
 
-        super(Diff, self).update(other, ignore_errors)
+        self |= other
 
-    def difference_update(self, other, ignore_errors=False):
+    def reverse_merge(self, other):
         self._check_capable(other)
 
         for neglast, negexcl in iteritems(other.negexcls):
             self.__do_neglast(neglast, operator.__isub__, negexcl)
 
-        super(Diff, self).difference_update(other, ignore_errors)
+        self -= other
 
-    def add_literal(self, literal, ignore_errors=False):
+    def add_literal(self, literal, add_node=True):
         for neglast in literal.neglasts:
             self.__do_neglast(neglast, invoker.add(literal))
 
         if literal not in self.trunk.literals:
             self.literals.add(literal)
-        if literal.node not in self.trunk.nodes:
+        if add_node and literal.node not in self.trunk.nodes:
             self.nodes.add(literal.node)
-
-        self._error_check(ignore_errors)
-
-    def sync_with_trunk(self, ignore_errors=False):
-        """Keep self a strict diff with the trunk."""
-        trunk = self.trunk
-
-        for neglast in self.negexcls:
-            self.__do_neglast(neglast, operator.__isub__,
-                              neglast.literals-trunk.neglefts[neglast])
-
-        super(Diff, self).difference_update(trunk, ignore_errors)
 
     def __do_neglast(self, neglast, op=None, *args):
         trunk_negleft = self.trunk.neglefts[neglast]
@@ -255,10 +276,14 @@ class Diff(Solution):
             self.todo.add(neg_literal)
 
     def iter_todo_away(self):
-        return filternot(self.literals.__contains__, pop_iter(self.todo))
+        literals = self.literals
+        trunk_literals = self.trunk.literals
+        return (literal for literal in pop_iter(self.todo)
+                if (literal not in literals and
+                    literal not in trunk_literals))
 
     def __repr__(self):
-        return ('<{cls.__name__}: {nr_todos} todo ({state})>'
+        return ('<{cls.__name__} ({state}/{nr_todos})>'
                 .format(cls=type(self),
                         nr_todos=len(self.todo),
                         state='valid' if self.valid else 'dead'))
@@ -269,23 +294,17 @@ class Branch(Diff):
 
     _dump_attrs = (Diff._dump_attrs + 'gen_literals'.split())
 
-    @property
-    def valid(self):
-        return self.error is None and len(self.nodes) == len(self.literals)
-
     def __init__(self, trunk, gen_literal):
         super(Branch, self).__init__(trunk)
         self.gen_literals = set()
 
-        self.error = None
         self.gen_literals.add(gen_literal)
-
         self.add_literal(gen_literal)
 
         self.reasons |= gen_literal.imply_reasons
         self.todo    |= gen_literal.implies
 
-    def update(self, other, ignore_errors=False):
+    def merge(self, other):
         if self.literals >= other.gen_literals:  # other is already in self
             assert self.nodes    >= other.nodes
             assert self.literals >= other.literals
@@ -295,11 +314,11 @@ class Branch(Diff):
                        for neglast, negexcl in iteritems(other.negexcls))
             return
 
-        super(Branch, self).update(other, ignore_errors)
+        super(Branch, self).merge(other)
 
     def __repr__(self):
         try:
-            return ('<{cls.__name__}: {nr_todos} todo ({state}) {gen_list!r}>'
+            return ('<{cls.__name__} ({state}/{nr_todos}) {gen_list!r}>'
                     .format(cls=type(self),
                             gen_list=list(self.gen_literals),
                             nr_todos=len(self.todo),
@@ -410,7 +429,7 @@ def create_trunk(pgraph, initial_literals=[]):
     return trunk
 
 
-def expand_branch(branch, ignore_errors=False):
+def expand_branch(branch):
     """Handles all branch todos (if any), in other words makes it ready.
 
     Implementation of non-recursive DFS."""
@@ -424,7 +443,7 @@ def expand_branch(branch, ignore_errors=False):
     def stack_push(branch):
         assert not hasattr(branch, 'todo_it'), ("A branch has 'todo_it' attr "
                                                 "iff it is already in stack")
-        branch.todo_it = trunk.iter_branch_todo_away(branch, ignore_errors)
+        branch.todo_it = trunk.iter_branch_todo_away(branch)
         stack.append(branch)
 
     def stack_pop():
@@ -442,86 +461,49 @@ def expand_branch(branch, ignore_errors=False):
 
         try:
             literal, implied = next(branch.todo_it)
-            logger.debug('\t%stodo literal: %r, implied: %r', log_indent,
+            logger.debug('\t%s todo literal: %r, implied: %r', log_indent,
                          literal, implied)
-
-            if implied is None:
-                assert ignore_errors
-                branch.add_literal(literal, ignore_errors)
-                continue
-
-            if not ignore_errors and not implied.valid:
-                logger.debug('\t%s(implied is not valid: %r)', log_indent,
-                             implied)
-                branch.todo.add(literal)  # it was NOT handled, save it back
-                raise SolutionError(branch)
-
-            if implied.ready:
-                branch.update(implied, ignore_errors)
-                continue
 
             if hasattr(implied, 'todo_it'):  # equivalent (mutual implication)
                 logger.debug('\t%s(mutual implication with %r)', log_indent,
                              implied)
                 implied.todo |= branch.todo
-                branch.todo.clear()  # otherwise update() would refuse it
+                branch.todo.clear()  # otherwise merge() would refuse it
 
-                try:
-                    implied.update(branch, ignore_errors)
-                finally:
-                    trunk.substitute_branch(branch, implied)
-                    branch.dispose()  # make gc happy
+                implied.merge(branch)
 
-                raise StopIteration  # forget about this branch
-
-        except SolutionError as error:
-            assert not ignore_errors, "Hey, no errors when ignore_errors=True!"
-            logger.debug('\t%sinviable  %r', log_indent, branch)
-
-            # unwind implication stack
-            for implicant in pop_iter(stack, pop=stack_pop):
-                implicant.error = error
-                error = SolutionError(implicant, error)
+                trunk.substitute_branch(branch, implied)
+                branch.dispose()  # forget about this branch and make gc happy
+                raise StopIteration
 
         except StopIteration:
             logger.debug('\t%ssucceeded %r', log_indent, branch)
-
-            # no more implications, or the branch was merged into an equivalent
             stack_pop()
 
-        else:  # defer until a branch is initialized
-            logger.debug('\t%sdeferred  %r', log_indent, branch)
-            #
-            # During initialization of the implied branch it may have been
-            # replaced by an implicant (appears upper on the stack).
-            #
-            # Example:
-            #   A => B => C => A
-            #           ^- assuming we're handling this implication now
-            #
-            # Upon returning back to handling the implication, a branch
-            # initially created for C gets replaced by A and should not be
-            # used anymore.
-            #
-            # So the best thing we can do here is to restart handling the
-            # literal from the beginning.
-            branch.todo.add(literal)
+        else:
+            if implied is None or not implied.valid:
+                logger.debug('\t%s(implied is not valid: %r)', log_indent,
+                             implied)
+                branch.add_literal(literal, add_node=False)
 
-            # what to handle next
-            stack_push(implied)
+            elif implied.ready:
+                branch.merge(implied)
+
+            else:
+                logger.debug('\t%sdeferred  %r', log_indent, branch)
+                # The best thing we can do here is to put the literal back
+                # into todo set to restart handling it later with properly
+                # initialized (and possibly substituted) implied branch.
+                branch.todo.add(literal)
+
+                stack_push(implied) # what to handle next
 
 
-def expand_branchset(trunk, ignore_errors=False):
-    branchset = trunk.branchset()
-
-    if ignore_errors:
-        dead_branchset = set(itervalues(trunk.dead_branches))
-        for branch in dead_branchset:
-            branch.sync_with_trunk(ignore_errors=True)
-        branchset.update(dead_branchset)
+def expand_branchset(trunk):
+    branchset = trunk.branchset() | set(itervalues(trunk.dead_branches))
 
     for branch in filter(getter.trunked, branchset):
-        expand_branch(branch, ignore_errors)
+        expand_branch(branch)
 
 
 def branchset_to_resolve(trunk):
@@ -540,38 +522,39 @@ def resolve_branches(trunk, branches=None):
     if branches is None:
         branches = branchset_to_resolve(trunk)
 
-    logger.dump(trunk)
-
     while branches:
         logger.info('resolving %d branch(es)', len(branches))
+        logger.dump(trunk)
 
-        resolved = Diff(trunk)  # a patch created by merging together all diffs
+        resolved = Diff(trunk)  # created by merging together all diffs
 
         for branch in branches:
-            logger.debug('\tmerging: %r', branch)
-            resolved.update(branch)
+            logger.debug('\t+merge %r', branch)
+            for gen_literal in branch.gen_literals:
+                branch.reasons
+            resolved.merge(branch)
         expand_branch(resolved)  # handle todos, if any
 
-        # reintegrate it back into trunk (cannot fail, always succeeds)
-        trunk.update(resolved)
+        logger.dump(resolved)
+        if not resolved.valid:
+            logger.info('resolved is not valid, giving up')
+            raise SolutionError(resolved)
 
-        # remove resolved branches and their opposites from branchmap
-        for literal in resolved.literals:
-            del trunk.branchmap[literal]
-            trunk.dead_branches[~literal] = trunk.branchmap.pop(~literal)
+        # Reintegrate into trunk. This also removes resolved branches and
+        # their opposites (refused branches) from branchmap.
+        trunk.commit(resolved)
 
         # Maintain remaining branches to be strict diffs with just updated
         # trunk. This may involve new conflicts, i.e. new branches can be
-        # resolved, so we'll create a new list of branches to resolve next.
-
+        # resolved next.
         for branch in trunk.branchset():
-            assert branch.valid, 'only valid branches must have left'
-            branch.difference_update(resolved, ignore_errors=True)
+            logger.debug('\t-merge %r', branch)
+            branch.reverse_merge(resolved)
         expand_branchset(trunk)
 
-        logger.dump(trunk)
-
         branches = branchset_to_resolve(trunk)
+
+    logger.dump(trunk)
 
 
 @logger.wrap
