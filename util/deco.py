6cf5464291f368c1d70821252135aab1eb801691
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-15 13:42:14 +0000

    (minor) fix reference in a comment

--- util/deco.py
+++ util/deco.py
@@ -31,7 +31,7 @@ def constructor_decorator(cls, **kwargs):
     # replaces itself with the original one and replaces the returned class
     # with 'cls' with the original __call__ method.
     #
-    # This magic is similar to the one used in util.compat.with_metaclass
+    # This magic is similar to the one used in compat_.with_meta
     # function.
     mcls = type(cls)
 
