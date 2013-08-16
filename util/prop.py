6e167a530a247c9ec36f3fb759e4f184a3ad751d
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-16 13:53:36 +0000

    util.prop: add doctests

--- util/prop.py
+++ util/prop.py
@@ -7,7 +7,31 @@ from _compat import *
 
 
 class default_property(object):
-    """Non-data descriptor."""
+    """Non-data descriptor.
+
+    Delegates to func everytime a property is accessed unless someone
+    explicitly overrides it by setting a new value.
+
+    Usage example:
+
+    >>> class C(object):
+    ...     @default_property
+    ...     def default(self):
+    ...         print("Accessing {cls.__name__}.default"
+    ...               .format(cls=type(self)))
+    ...         return 17
+    ...
+    >>> x = C()
+    >>> x.default
+    Accessing C.default
+    17
+    >>> x.default
+    Accessing C.default
+    17
+    >>> x.default = 42
+    >>> x.default
+    42
+    """
     __slots__ = 'func'
 
     def __init__(self, func):
@@ -21,7 +45,28 @@ class default_property(object):
 
 
 class cached_property(default_property):
-    """Non-data descriptor."""
+    """Non-data descriptor.
+
+    Delegates to func only the first time a property is accessed.
+
+    Usage example:
+
+    >>> class C(object):
+    ...     @cached_property
+    ...     def cached(self):
+    ...         print("Accessing {cls.__name__}.cached".format(cls=type(self)))
+    ...         return 17
+    ...
+    >>> x = C()
+    >>> x.cached
+    Accessing C.cached
+    17
+    >>> x.cached
+    17
+    >>> x.cached = 42
+    >>> x.cached
+    42
+    """
     __slots__ = ()
 
     def __get__(self, obj, objtype=None):
@@ -33,19 +78,52 @@ class cached_property(default_property):
 
 
 class intercepting_property(property):
-    """Data descriptor."""
-    __slots__ = 'name'
+    """Data descriptor.
+    Usage example:
+
+    >>> class C(object):
+    ...     @intercepting_property
+    ...     def icp(self, value):
+    ...         print("Get {cls.__name__}.icp: {value}"
+    ...               .format(cls=type(self), **locals()))
+    ...         return -value
+    ...     @icp.setter
+    ...     def icp(self, value):
+    ...         print("Set {cls.__name__}.icp: {value}"
+    ...               .format(cls=type(self), **locals()))
+    ...         return -value
+    ...     @icp.deleter
+    ...     def icp(self):
+    ...         print("Del {cls.__name__}.icp".format(cls=type(self)))
+    ...
+    >>> x = C()
+    >>> x.icp
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'C' object has no attribute 'icp'
+    >>> x.icp = 7
+    Set C.icp: 7
+    >>> x.__dict__['icp']
+    -7
+    >>> x.icp
+    Get C.icp: -7
+    7
+    >>> del x.icp
+    Del C.icp
+    >>> 'icp' in x.__dict__
+    False
+    """
 
     def __init__(self, fget=None, fset=None, fdel=None, name=None, doc=None):
-        func = fget or fset or fdel
+        super(intercepting_property, self).__init__(fget, fset, fdel, doc)
 
         if name is None:
-            if func is not None:
-                name = func.__name__
-            else:
+            func = fget or fset or fdel
+            if func is None:
                 raise TypeError('no attribute name given')
+            name = func.__name__
 
-        super(intercepting_property, self).__init__(fget, fset, fdel, doc)
+        self.name = name
 
     def __get__(self, obj, objtype=None):
         if obj is None:
@@ -53,7 +131,8 @@ class intercepting_property(property):
         try:
             ret = obj.__dict__[self.name]
         except KeyError:
-            raise AttributeError
+            raise AttributeError("'{objtype.__name__}' object has no "
+                                 "attribute '{self.name}'".format(**locals()))
         fget = self.fget
         if fget is not None:
             ret = fget(obj, ret)
@@ -72,6 +151,12 @@ class intercepting_property(property):
         try:
             del obj.__dict__[self.name]
         except KeyError:
-            raise AttributeError
+            objtype = type(obj)
+            raise AttributeError("'{objtype.__name__}' object has no "
+                                 "attribute '{self.name}'".format(**locals()))
 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
