3fccbf54d1442d562d4c21ea6385937d2bd74395
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-26 22:35:28 +0000

    mywaf: unbind my_load function from waf context, rename it to load_namespace, remove MyWafModuleMixin

--- mywaf.py
+++ mywaf.py
@@ -29,67 +29,43 @@ from util.deco import defer_call
 from collections import deque
 
 from waflib import Context as wafcontext
-from waflib import Utils   as wafutils
 from waflib import Errors  as waferrors
+from waflib import Node    as wafnode
+from waflib import Utils   as wafutils
 
 
-def wafcontext_method(func):
+def ctx_method(func):
     setattr(wafcontext.Context, func.__name__, func)
     return func
+wafcontext.ctx_method = ctx_method
 
 
 DEFAULT_LOADERS = [MyDslLoader, PyDslLoader]
+RUN_PATH_NODE   = wafnode.Node('', None).find_dir(wafcontext.run_dir)
 
-@wafcontext_method
-def mybuild(ctx, conf_name,
-            path=None, loaders=DEFAULT_LOADERS,
-            recurse_name=None):
-    """Facade function for the whole Mybuild machinery.
-
-    Loads all Mybuild files found in path using given loaders, resolves
-    a configuration specified by conf_name and recurses into each enabled
-    module.
+def load_namespace(namespace, path=None, loaders=DEFAULT_LOADERS):
+    """Loads all Mybuild files found in path using given loaders into the
+    specified namespace.
 
     Args:
-        conf_name (str): a fully qualified name of the configuration including
-            a namespace, e.g. 'ns.path.to.conf'.
+        namespace (str)
         path (list/str): a list of strings (or a string of space-separated
             entries) denoting directories to search for Mybuild files.
-            If not specified, ctx.path is used instead.
+            If not specified, RUN_PATH_NODE is used instead.
         loaders (iterable/mapping): a list of loaders used to recognize and
             load Mybuild files, or a dictionary mapping such loaders to
             their initializing values (loader-specific).
-        recurse_name (str):
-            Name of method to invoke on each resolved module. Defaults to
-            the name of current context.
 
     Returns:
-        The namespace root wrapped by a module instance accessor
-        (see MybuildInstanceAccessor).
+        The namespace root module.
     """
-    namespace, _, conf_relname = conf_name.partition('.')
-    if not conf_relname:
-        raise ValueError("conf_name must be a qualified name "
-                         "(including a namespace): '%s'" % conf_name)
-    else:
-        conf_getter = attrgetter(conf_relname)
-
-    ns_pymodule = ctx.my_load(namespace, path, loaders)
-    instance_map = ctx.my_resolve(conf_getter(ns_pymodule))
-    ctx.my_recurse(sorted(itervalues(instance_map), key=str))
-
-    return MybuildInstanceAccessor(ns_pymodule, instance_map)
-
-
-@wafcontext_method
-def my_load(ctx, namespace, path=None, loaders=DEFAULT_LOADERS):
     loaders_init = (dict if is_mapping(loaders) else dict.fromkeys)(loaders)
 
     if path is not None:
         path = wafutils.to_list(path)
-        path_nodes = [ctx.path.find_node(entry) for entry in path]
+        path_nodes = [RUN_PATH_NODE.find_node(entry) for entry in path]
     else:
-        path_nodes = [ctx.path]
+        path_nodes = [RUN_PATH_NODE]
     path = [node.abspath() for node in path_nodes]
 
     loader_files_glob = ['**/' + loader_filename(l) for l in loaders_init]
@@ -109,47 +85,66 @@ def my_load(ctx, namespace, path=None, loaders=DEFAULT_LOADERS):
                       loaders_init)
 
 
-@wafcontext_method
+@wafcontext.ctx_method
+def mybuild(ctx, conf_module, recurse_name=None):
+    """Facade function for the whole Mybuild machinery.
+
+    Resolves a configuration specified by conf_module and recurses into each
+    enabled module.
+
+    Args:
+        conf_module (mybuild.core.Module): the configuration to resolve.
+        recurse_name (str): Name of method to invoke on each resolved module.
+            Defaults to the name of current context.
+
+    Returns:
+        The namespace root wrapped by a module instance accessor
+        (see MybuildInstanceAccessor).
+    """
+    instances = ctx.my_resolve(conf_module)
+    return ctx.my_recurse(sorted(instances, key=str))
+
+
+@wafcontext.ctx_method
 def my_resolve(ctx, conf_module):
     cache = ctx._my_resolve_cache
     try:
-        instance_map = cache[conf_module]
+        instances = cache[conf_module]
     except KeyError:
         try:
-            instances = resolve(conf_module, module_meta=mywaf_module_meta)
+            instances = resolve(conf_module)
         except SolveError as e:
             e.rgraph = get_error_rgraph(e)
             print_graph(e.rgraph)
             raise e
-        
-        instance_map = dict((instance._module, instance)
-                            for instance in instances)
-        cache[conf_module] = instance_map
 
-    return instance_map
+        cache[conf_module] = instances
 
-wafcontext.Context._my_resolve_cache = {}  # {conf_module: instance_map}
+    return instances
+
+wafcontext.Context._my_resolve_cache = {}  # {conf_module: instances}
+
 
 def print_graph(rgraph):
     """
-    Simple way to print reason graph. Nodes of more one reason are printed 
+    Simple way to print reason graph. Nodes of more one reason are printed
     in new line without offset.
     """
     node_deque = deque()
     used = set()
-        
+
     def dfs(node, reason, depth):
         if node in used:
             print_reason(reason,depth)
             return
-        
+
         used.add(node)
         print_reason(reason,depth)
         for cons in node.therefore:
             dfs(cons, node.therefore[cons], depth + 1)
             for container in cons.containers:
                 process_container(container)
-                            
+
     def process_container(container):
         if container in used:
             return
@@ -157,35 +152,36 @@ def print_graph(rgraph):
         for ccons in container.therefore:
             if ccons not in node_deque:
                 node_deque.appendleft((ccons, container.therefore[ccons]))
-                            
+
     def print_reason(reason, depth):
         print '  ' * depth, reason
         if not reason.follow:
             return
-            
-        literal = None 
+
+        literal = None
         if reason.literal is not None:
             literal = ~reason.literal
         else:
             literal = reason.cause_literals[0]
-        
+
         assert literal in rgraph.violation_graphs
-                
+
         print '---dead branch {0}---------'.format(literal)
-        print_graph(rgraph.violation_graphs[literal])    
-        print '---------dead branch {0}---'.format(literal)        
-        
+        print_graph(rgraph.violation_graphs[literal])
+        print '---------dead branch {0}---'.format(literal)
+
     #node_deque contains touples (node, reason)
     for node in rgraph.initial.therefore:
         node_deque.append((node, rgraph.initial.therefore[node]))
         process_container(node)
-        
+
     while node_deque:
-        node, reason = node_deque.pop()        
+        node, reason = node_deque.pop()
         dfs(node, reason, 0)
 
-@wafcontext_method
-def my_recurse(ctx, instances, name=None, mandatory=True):
+
+@wafcontext.ctx_method
+def my_recurse(ctx, instances, name=None, mandatory=False):
     if name is None:
         name = ctx.fun
 
@@ -231,34 +227,6 @@ class MybuildInstanceAccessor(object):
         except KeyError:
             cls = type(self)
             return cls(retobj, retmap)
-
-
-def mywaf_module_meta(name, bases, attrs, **kwargs):
-    return new_type(name, bases + (MyWafModuleMixin,), attrs, **kwargs)
-
-
-class MyWafModuleMixin(object):
-    """docstring for MyWafModuleMixin"""
-
-    @defer_call
-    def load(ctx, *args, **kwargs):
-        ctx.load(*args, **kwargs)
-
-    @defer_call
-    def bld(ctx, *args, **kwargs):
-        ctx(*args, **kwargs)
-
-    def options(self, ctx):
-        print('mywaf options: %r' % self)
-        self.load.call_on(ctx)
-
-    def configure(self, ctx):
-        print('mywaf configure: %r' % self)
-        self.load.call_on(ctx)
-
-    def build(self, bld):
-        print('mywaf build: %r' % self)
-        self.bld.call_on(bld)
 
 
 def init(ctx):
