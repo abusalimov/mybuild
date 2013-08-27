1730948088fab7ba76ecc0c2202054c09cebc577
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-08-27 13:53:15 +0000

    fix solver test, add wafctx attribute

--- test/module_tests_solver.py
+++ test/module_tests_solver.py
@@ -1,23 +1,19 @@
 import unittest
-from unittest import TestCase
 
 from mybuild.binding.pydsl import module
-from mybuild.context import Context
+from mybuild.context import resolve
 from mybuild.solver import solve
 from mybuild.solver import SolveError
-import mywaf
 
-class SolverTestCase(TestCase):
+class SolverTestCase(unittest.TestCase):
 
     def test_simple_solution(self):
-        context = Context()
-
         @module
         def conf(self):
-            self._constrain(m1(isM2 = True))
+            self._constrain(m1(isM2=True))
 
         @module
-        def m1(self, isM2 = False):
+        def m1(self, isM2=False):
             if isM2:
                 self._constrain(m2)
 
@@ -25,22 +21,19 @@ class SolverTestCase(TestCase):
         def m2(self):
             pass
 
-        instances = context.resolve(conf)
-        modules = set(map(type, instances))
-        
+        modules = resolve(conf)
+
         self.assertIn(conf, modules)
         self.assertIn(m1, modules)
         self.assertIn(m2, modules)
 
     def test_unused_module(self):
-        context = Context()
-
         @module
         def conf(self):
-            self._constrain(m1(isM2 = True))
+            self._constrain(m1(isM2=True))
 
         @module
-        def m1(self, isM2 = False):
+        def m1(self, isM2=False):
             if isM2:
                 self._constrain(m2)
 
@@ -57,9 +50,8 @@ class SolverTestCase(TestCase):
         def m4(self):
             pass
 
-        instances = context.resolve(conf)
-        modules = set(map(type, instances))
-        
+        modules = resolve(conf)
+
         self.assertIn(conf, modules)
         self.assertIn(m1, modules)
         self.assertIn(m2, modules)
@@ -67,8 +59,6 @@ class SolverTestCase(TestCase):
         self.assertNotIn(m4, modules)
 
     def test_cyclic_dependence(self):
-        context = Context()
-
         @module
         def conf(self):
             self._constrain(m1)
@@ -81,59 +71,56 @@ class SolverTestCase(TestCase):
         def m2(self):
             self._constrain(m1)
 
-        instances = context.resolve(conf)
-        modules = set(map(type, instances))
-        
+        modules = resolve(conf)
+
         self.assertIn(conf, modules)
         self.assertIn(m1, modules)
         self.assertIn(m2, modules)
-        
+
     def test_parameter_violation_error(self):
-        context = Context()
-        
         @module
         def conf(self):
-            self._constrain(m1(a = True))
-            self._constrain(m1(a = False))
+            self._constrain(m1(a=True))
+            self._constrain(m1(a=False))
 
         @module
-        def m1(self, a = False):
+        def m1(self, a=False):
             pass
-        
-        context.consider(conf)
-        g = context.create_pgraph()
-        
+
         with self.assertRaises(SolveError):
-            mywaf.my_resolve(context, conf)
+            self.wafctx.my_resolve(conf)
 
     def test_solve(self):
         #(~A | A&~A)
-        context = Context()
-        
         @module
         def conf(self):
             self._constrain(m1)
 
         @module
-        def m1(self, a = False):
+        def m1(self, a=False):
             if a:
                 self._constrain(m2)
             else:
                 self._constrain(m3(a=False))
-    
+
         @module
         def m2(self):
             self._constrain(m3(a=True))
             self._constrain(m3(a=False))
-        
+
         @module
-        def m3(self, a = False):
+        def m3(self, a=False):
             pass
-            
-        instances = context.resolve(conf)
-        modules = set(map(type, instances))
-        
+
+        modules = resolve(conf)
+
         self.assertIn(conf, modules)
         self.assertIn(m1, modules)
         self.assertNotIn(m2, modules)
         self.assertIn(m3, modules)
+
+
+def suite(wafctx_):
+    class WafCtxBoundTestCase(SolverTestCase):
+        wafctx = wafctx_
+    return unittest.TestLoader().loadTestsFromTestCase(WafCtxBoundTestCase)
