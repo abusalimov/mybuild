8b9d33e94f28b7f47bcb79a3e39beb9d69be3a4f
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-09-05 22:56:29 +0000

    mydsl: everything is expressed through pydsl logic

--- mybuild/binding/mydsl.py
+++ mybuild/binding/mydsl.py
@@ -13,57 +13,19 @@ from _compat import *
 import functools
 
 from mybuild import core
-from util.deco import constructor_decorator
-from util.prop import class_instance_method
+from mybuild.binding import pydsl
+from util.deco import class_from_constructor
 
 
-class MyDslModuleTypeStub(object):
-    """Stub for a Module type which is converted into a real type upon
-    a call to __my_new__."""
-
-    _module_bases = [core.Module]
-
-    def __init__(self, *args, **kwargs):
-        """Args/kwargs are converted into a list of options."""
-        super(MyDslModuleTypeStub, self).__init__()
-
-        for optype in args:
-            if not isinstance(optype, core.Optype):
-                raise TypeError('Positional argument must be an option: '
-                                '{optype}'.format(**locals()))
-            if not hasattr(optype, '_name'):
-                raise TypeError('Positional option must provide a name '
-                                'explicitly')
-
-        optypes = list(args)
-
-        for name, optype in iteritems(kwargs):
-            if not isinstance(optype, core.Optype):
-                optype = core.Optype(optype)
-            if not hasattr(optype, '_name'):
-                optype.set(name=name)
-
-            optypes.append(optype)
-
-        self.optypes = optypes
-
-    @class_instance_method
-    def __my_new__(cls, self, init_func):
-        if self is None:
-            # let 'module() {...}' and 'module {...}' to behave the same way
-            return cls().__my_new__(init_func)
-
-        @functools.wraps(init_func)
-        def module(self, *args, **kwargs):
-            init_func(self)
-
-        return self._module_from_constructor(module)
-
-    @property
-    def _module_from_constructor(self):
-        return constructor_decorator(*self._module_bases, optypes=self.optypes)
+class MyDslModuleMeta(pydsl.PyDslModuleMeta):
+    __my_new__ = class_from_constructor
 
 
-module = MyDslModuleTypeStub
+class MyDslModule(extend(pydsl.PyDslModule,
+                         metaclass=MyDslModuleMeta, internal=True)):
+    pass
+
+
+module = MyDslModule
 option = core.Optype
 
