c89a90382f0e9505681badd33570334d4e6dcbf7
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-07-05 04:33:35 +0400

    inherit: mro() and kicking mro recalc method

--- util/inherit.py
+++ util/inherit.py
@@ -6,7 +6,24 @@ from _compat import *
 
 
 class InheritMetaBase(type):
-    pass
+
+    def _kick_mro_update(cls):
+        cls.__bases__ = cls.__bases__
+
+    def mro(cls):
+        meta = type(cls)
+        for base in cls.__bases__:
+            base_meta = type(base)
+            if not issubclass(meta, base_meta):
+                # Impose more strict requirements on changing __bases__,
+                # see: http://bugs.python.org/issue21919
+                raise TypeError("metaclass conflict: "
+                                "the metaclass of a derived class "
+                                "must be a (non-strict) subclass "
+                                "of the metaclasses of all its bases")
+
+        return super(InheritMetaBase, cls).mro()
+
 
 class InheritOwnerMeta(InheritMetaBase):
     pass
