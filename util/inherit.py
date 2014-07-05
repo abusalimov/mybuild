0e75046ecc40eee3bdf762e2bad3cd90b8752999
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-07-05 05:18:38 +0400

    inherit: calculating new bases for an owned attr

--- util/inherit.py
+++ util/inherit.py
@@ -3,6 +3,8 @@ Dynamic inheritance for class members.
 """
 
 from _compat import *
+from util.itertools import pop_iter
+from util.itertools import unique
 
 
 class InheritMetaBase(type):
@@ -26,8 +28,38 @@ class InheritMetaBase(type):
 
 
 class InheritOwnerMeta(InheritMetaBase):
-    pass
+
+    def __new__(mcls, name, bases, attrs):
+        cls = super(InheritOwnerMeta, mcls).__new__(mcls, name, bases, attrs)
+
+        for attr, value in iteritems(cls.__dict__):
+            if isinstance(value, InheritValueMeta):
+                if value._inherit_owner is not None:
+                    raise ValueError
+
+                cls.__inherit_attr(attr, value)
+
+        return cls
+
+    def __inherit_attr(cls, attr, value):
+        value._inherit_owner = cls
+        value.__bases__ = cls.__bases_for_attr(attr)
+
+    def __bases_for_attr(cls, attr):
+        base_ids = set()
+        todo = list(cls.__bases__)
+        for base in unique(pop_iter(todo)):
+            if attr in base.__dict__:
+                base_ids.add(id(base))
+            else:
+                todo += base.__bases__
+
+        return [base for base in cls.__mro__ if id(base) in base_ids]
+
 
 class InheritValueMeta(InheritMetaBase):
-    pass
+
+    def __init__(cls, name, bases, attrs):
+        super(InheritValueMeta, cls).__init__(name, bases, attrs)
+        cls._inherit_owner = None
 
