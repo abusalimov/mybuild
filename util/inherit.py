cf3245ca6f622d32d2d4c9e6acd91954ad3e80c2
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-07-06 05:46:10 +0400

    inherit: setattr and delattr, plus uninheriting an old value

--- util/inherit.py
+++ util/inherit.py
@@ -9,7 +9,7 @@ from util.itertools import unique
 
 class InheritMeta(type):
 
-    def _kick_mro_update(cls):
+    def __kick_mro_update(cls):
         cls.__bases__ = cls.__bases__
 
     def mro(cls):
@@ -32,34 +32,68 @@ class InheritMeta(type):
                                 "must be a (non-strict) subclass "
                                 "of the metaclasses of all its bases")
 
-    def __check_owner(cls, attr, value):
+    def __check_owner(cls, attr, value, target_cls=None):
         try:
-            owner_cls, owner_attr = value.__owner
+            owner_cls, owner_attr, orig_bases = value.__owner
         except AttributeError:
             return
 
-        if owner_cls is not cls or owner_attr != attr:
-            raise ValueError("Can't inherit an owned value '{value}': "
-                             "owned by '{owner_cls}' "
-                             "through '{owner_attr}' attribute"
-                             .format(**locals()))
+        if target_cls is None:
+            target_cls = cls
+
+        if target_cls is owner_cls and attr == owner_attr:
+            return orig_bases
+
+        raise ValueError("Can't inherit value '{value}' "
+                         "already owned by '{owner_cls}' "
+                         "through '{owner_attr}' attribute"
+                         .format(**locals()))
 
     def __inherit_attr(cls, attr, value):
-        cls.__check_owner(attr, value)
+        if cls.__check_owner(attr, value) is None:
+            value.__owner = cls, attr, value.__bases__
+            try:
+                value.__bases__ = cls.__bases_for_attr(attr)
+            except:
+                del value.__owner  # __bases__ is rolled back automatically
+                raise
 
-        value.__owner = cls, attr
-        value.__bases__ = cls.__bases_for_attr(attr)
+    def __uninherit_attr(cls, attr, value):
+        orig_bases = cls.__check_owner(attr, value)
+        if orig_bases is not None:
+            value.__bases__ = orig_bases
+            del value.__owner
 
     def __bases_for_attr(cls, attr):
-        base_ids = set()
+        base_values = dict()  # {id(base): value}
+
+        check_owner = cls.__check_owner
         todo = list(cls.__bases__)
         for base in unique(pop_iter(todo)):
             if attr in base.__dict__:
-                base_ids.add(id(base))
+                value = base.__dict__[attr]
+                check_owner(attr, value, base)
+                base_values[id(base)] = value
             else:
                 todo += base.__bases__
 
-        return [base for base in cls.__mro__ if id(base) in base_ids]
+        return [base_values[base_id] for base_id in map(id, cls.__mro__)
+                if base_id in base_values]
+
+    def __setattr__(cls, attr, value):
+        cls.__check_owner(attr, value)
+        try:
+            cls.__uninherit_attr(attr, cls.__dict__.get(attr))
+            super(InheritMeta, cls).__setattr__(attr, value)
+        finally:
+            cls.__kick_mro_update()  # will find the value in cls.__dict__
+
+    def __delattr__(cls, attr):
+        try:
+            cls.__uninherit_attr(attr, cls.__dict__.get(attr))
+            super(InheritMeta, cls).__delattr__(attr)
+        finally:
+            cls.__kick_mro_update()
 
 
 def is_inherit_value(value):
