ecba9019966e7a422238a9cd8ab8f9deb39a50bf
Vita Loginova <vita.loginova@gmail.com>
2015-01-09 19:23:24 +0400

    add helloworld example

---
+++ example/helloworld/src/main.c
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * @date Jun 25, 2013
+ * @author: Anton Bondarev
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello from Mybuild v%s!\n", MYBUILD_VERSION);
+	return 0;
+}
