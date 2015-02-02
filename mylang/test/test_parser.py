fb4b509968071832d71631aeb2e88fab2436672c
Vita Loginova <vita.loginova@gmail.com>
2015-02-02 20:20:18 +0400

    mylang: test: Add ASTComparator class and simple test for parser
    Node equality (copy-paste from the class doc):
    A node a in an AST A is equal to a node b in an AST B <=>
            1. The node a has the same type as the node b;
            2. The node a appears in the same place in the AST A as
               the node b appears in the AST B and vice versa;
            3. Each child of the node a is equal to some child of the
               node b and vice versa.

---
+++ mylang/test/test_parser.py
@@ -0,0 +1,108 @@
+"""
+Unit tests for mylang.parse
+"""
+
+__author__ = "Vita Loginova"
+__date__ = "2015-02-02"
+
+from _compat import *
+
+import ast
+import itertools
+
+import unittest
+
+from mylang.parse import my_parse
+
+
+class ASTComparator(object):
+    """
+    A node a in an AST A is equal to a node b in an AST B <=>
+        1. The node a has the same type as the node b;
+        2. The node a appears in the same place in the AST A as the node
+           b appears in the AST B and vice versa;
+        3. Each child of the node a is equal to some child of the node b and
+           vice versa.
+    """
+    def __init__(self):
+        super(ASTComparator, self).__init__()
+        self.names_a = {}
+        self.names_b = {}
+
+    def compare(self, node_a, node_b):
+        # This function is not reentrant as it fills self.names_a and
+        # self.names_b
+        if type(node_a) != type(node_b):
+            return False
+
+        if isinstance(node_a, ast.AST):
+            for k, v in vars(node_a).iteritems():
+                if k in ('lineno', 'col_offset', 'ctx'):
+                    continue
+                if not self.compare(v, getattr(node_b, k)):
+                    return False
+            return True
+
+        if isinstance(node_a, list):
+            return all(itertools.starmap(self.compare,
+                                        itertools.izip(node_a, node_b)))
+
+        if isinstance(node_a, str):
+
+            if node_a in self.names_a and node_b in self.names_b:
+                return self.names_a[node_a] == node_b
+
+            elif node_a not in self.names_a and node_b not in self.names_b:
+                self.names_a[node_a] = node_b
+                self.names_b[node_b] = node_a
+                return True
+
+            else:
+                # print(node_a, node_b)
+                return False
+
+        return node_a == node_b
+
+
+class ParserTestCase(unittest.TestCase):
+
+    def test_empty_module(self):
+        py_source = """
+try:
+    @__my_exec_module__
+    def _main_():
+        global __name__
+        _module_ = __name__
+
+        def func(self):
+            return __my_new_type__(module, 'main', _module_, None, [])
+
+        return [('main', func, False)]
+
+        pass
+except __my_exec_module__:
+    pass
+"""
+
+        my_source = """
+module main: {
+}
+"""
+        py_node = ast.parse(py_source, mode='exec')
+        my_node = my_parse(my_source)
+
+        self.assertIs(True, ASTComparator().compare(my_node, py_node))
+
+
+def suite():
+    import sys
+    return unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+
+
+if __name__ == '__main__':
+    import util, sys, logging
+    # util.init_logging(filename='%s.log' % __name__)
+    util.init_logging(sys.stderr,
+                      level=logging.DUMP)
+
+    unittest.main()
