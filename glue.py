ef5d461ea99256477388777bbbee429fbd6461ac
Vita Loginova <vita.loginova@gmail.com>
2015-02-25 19:45:14 +0300

    glue: Use unique defines for each tool

--- glue.py
+++ glue.py
@@ -47,12 +47,18 @@ class CcTool(WafBasedTool):
     waf_tools = ['compiler_c']
 
     def create_namespaces(self, module):
-        return dict(cc=Namespace(defines={}))
+        return dict(cc=Namespace(defines=Namespace()))
+
+    def define(self, key, val):
+        format_str = '{0}=\"{1}\"' if isinstance(val, str) else '{0}={1}'
+        self.defines.append(format_str.format(key, val))
 
     def build(self, module, ctx):
         self.use = []
-        for name in module.cc.defines:
-            ctx.define(name, module.cc.defines[name])
+        self.defines = []
+
+        for k, v in iteritems(module.cc.defines.__dict__):
+            self.define(k, v)
 
         for m in module.depends:
             self.use.append(ctx.instance_map[m]._name)
@@ -61,22 +67,26 @@ class CcTool(WafBasedTool):
 class CcObjTool(CcTool):
     def build(self, module, ctx):
         super(CcObjTool, self).build(module, ctx)
-        ctx.objects(source=module.files, target=module._name, use=self.use)
+        ctx.objects(source=module.files, target=module._name,
+                    use=self.use, defines=self.defines)
 
 
 class CcAppTool(CcTool):
     def build(self, module, ctx):
         super(CcAppTool, self).build(module, ctx)
-        ctx.program(source=module.files, target=module._name, use=self.use)
+        ctx.program(source=module.files, target=module._name,
+                    use=self.use, defines=self.defines)
 
 
 class CcLibTool(CcTool):
     def build(self, module, ctx):
         super(CcLibTool, self).build(module, ctx)
         if module.isstatic:
-            ctx.stlib(source=module.files, target=module._name, use=self.use)
+            ctx.stlib(source=module.files, target=module._name,
+                    use=self.use, defines=self.defines)
         else:
-            ctx.shlib(source=module.files, target=module._name, use=self.use)
+            ctx.shlib(source=module.files, target=module._name,
+                    use=self.use, defines=self.defines)
 
 
 tool = Namespace(cc=CcObjTool(), cc_app=CcAppTool(), cc_lib=CcLibTool())
