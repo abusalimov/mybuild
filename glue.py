abusalimov/mybuild

commit 7715d0b77f7ed8cd8a238e56e05b4e89541bf3b4

glue: Add characters escaping in tool.define()

glue.py
@@ -58,8 +58,11 @@ class CcTool(WafBasedTool):
 
     def define(self, key, val):
         assert('defines' in self.build_kwargs)
-        format_str = '{0}=\"{1}\"' if isinstance(val, str) else '{0}={1}'
-        self.build_kwargs['defines'].append(format_str.format(key, val))
+        if isinstance(val, str):
+            import json
+            val = json.dumps(val).replace("\\u0007", "\a") \
+                                 .replace("\\u000b", "\v")
+        self.build_kwargs['defines'].append('{0}={1}'.format(key, val))
 
     def build(self, module, ctx):
         self.build_kwargs['use'] = [m._name for m in module.depends]

commit facca3503061f9f69bc835ac1f01c608d04dbed2

util,glue: Extract C string escaping function
Also fix improper escaping of '\a' and '\v' chars.

Fixes: 7715d0b ("glue: Add characters escaping in tool.define()")

glue.py
@@ -17,6 +17,7 @@ from mybuild.binding import pydsl
 from util.operator import attr
 from util.namespace import Namespace
 from util.prop import cached_property
+from util.misc import stringify
 
 
 class LoaderMixin(object):
@@ -59,9 +60,7 @@ class CcTool(WafBasedTool):
     def define(self, key, val):
         assert('defines' in self.build_kwargs)
         if isinstance(val, str):
-            import json
-            val = json.dumps(val).replace("\\u0007", "\a") \
-                                 .replace("\\u000b", "\v")
+            val = stringify(val)
         self.build_kwargs['defines'].append('{0}={1}'.format(key, val))
 
     def build(self, module, ctx):

util/misc.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 from _compat import *
 
 from collections import namedtuple as _namedtuple
+import json as _json
 
 from util.collections import is_mapping
 
@@ -33,6 +34,12 @@ def to_dict(iterable_or_mapping, check_exclusive=False):
         raise ValueError('Item(s) with conflicting keys detected')
 
     return ret_dict
+
+
+def stringify(s):
+    return (_json.dumps(s)
+            .replace(r'\u0007', r'\a')
+            .replace(r'\u000b', r'\v'))
 
 
 if hasattr(0, 'bit_length'):