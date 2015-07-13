625a9e9b0928ab6a2a3c18ee277c875b4be0dbbd
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-07-13 03:12:03 +0300

    LICENSE: Switch to more clear and simple MIT license
    Also update the copyright years.

--- mybuild/__init__.py
+++ mybuild/__init__.py
@@ -4,7 +4,7 @@ TODO docs. -- Eldar
 """
 
 __author__ = "Eldar Abusalimov"
-__copyright__ = "Copyright 2012-2013, The Embox Project"
-__license__ = "New BSD"
+__copyright__ = "Copyright 2012-2015, Eldar Abusalimov"
+__license__ = "MIT"
 __version__ = "0.5"
 
